d7d6d84a631796d2cb7b1bb219dc636860857874
Sebastian <sebastiandripari@gmail.com>
2021-10-15 13:33:14 -0300

    fix heavy endpoint

--- app/app.js
+++ app/app.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const app = express();
-const TIMEOUT = 5000;
+const express = require('express')
+const app = express()
+const TIMEOUT = 5000
 
-app.get('/ping', function (req, res) {
-    res.send('pong\n');
-});
+app.get('/', function (_, res) {
+    res.send('Hello, world!')
+})
 
-app.get('/', function (req, res) {
-    res.send('Hello, world!');
-});
+app.get('/ping', function (_, res) {
+    res.send('pong\n')
+})
 
-app.get('/heavy', function (req, res){
-    let now = new Date();
-    while(new Date()-now<= TIMEOUT);
-    res.send('Hello, world!');
-});
+app.get('/heavy', function (_, res){
+    let now = new Date()
 
-app.listen(3000);
+    while (new Date() - now <= TIMEOUT) {
+    }
+
+    res.send('Heavy OK')
+})
+
+app.listen(3000)
