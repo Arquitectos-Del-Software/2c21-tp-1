ad511cfc4149c26fc85cd39d580e106dbc2ee207
Sebastian <sebastiandripari@gmail.com>
2021-10-20 22:24:48 -0300

    load tests

--- app/app.js
+++ app/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const fs = require('fs')
+const axios = require('axios')
+const request = require('sync-request')
+
 const app = express()
+
+const PORT = 3000
 const TIMEOUT = 100
+const FILENAME = 'archivo_ejemplo.txt'
 
 app.get('/', function (_, res) {
     res.send('Hello, world!')
@@ -17,6 +24,37 @@ app.get('/sync', function (_, res) {
     res.send('Sincronico OK')
 })
 
+app.get('/request_async', (_, res) => {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+        .then(response => {
+            res.send(response.data)
+        })
+        .catch(err => {
+            res.send(err)
+        })
+})
+
+app.get('/request_sync', async (_, res) => {
+    try {
+        const response = request('GET', 'https://dog.ceo/api/breeds/image/random')
+        res.send(response.getBody('utf8'))
+    } catch(err) {
+        res.send(err)
+    }
+})
+
+app.get('/read_file_sync', (_, res) => {
+    const data = fs.readFileSync(FILENAME, { encoding: 'utf8', flag: 'r' });
+    res.send(data)
+})
+
+app.get('/read_file_async', (_, res) => {
+    fs.readFile(FILENAME, { encoding: 'utf8', flag: 'r' },
+        (_, data) => {
+            res.send(data)
+        })
+})
+
 app.get('/async', function (_, res) {
     setTimeout(() => {
         res.send('Asincronico OK')
@@ -26,10 +64,11 @@ app.get('/async', function (_, res) {
 app.get('/heavy', function (_, res) {
     let now = new Date()
 
-    while (new Date() - now <= TIMEOUT) {
-    }
+    while (new Date() - now <= TIMEOUT) {}
 
     res.send('Heavy OK')
 })
 
-app.listen(3000)
+app.listen(PORT, () => {
+    console.info(`Listen on port ${PORT}`)
+})
